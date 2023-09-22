3ece2393b7ef06043d0f4de734afa12d0501b26a
ismailgiles <iagiles72@outlook.com>
2023-09-22 14:46:38 +0000

    added fake_ui and test_start_game files

---
+++ tests/robot/fake_ui.py
@@ -0,0 +1,5 @@
+from levelup.controller import GameController
+from levelup.ui import GameApp
+
+
+
---
+++ tests/test_start_game.py
@@ -0,0 +1,2 @@
+from unittest import TestCase
+from levelup.controller import GameController, Direction, InvalidMoveException
